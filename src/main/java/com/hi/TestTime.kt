80051ad0960dffd5da978e1d2b2fe97fc1872fca
hi-dhl <hihuilong@gmail.com>
2022-10-02 17:42:21 +0800

    添加编译成功和失败提示

---
+++ src/main/java/com/hi/TestTime.kt
@@ -0,0 +1,10 @@
+package com.hi
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2022/10/2
+ *     desc  : 
+ * </pre>
+ */class TestTime {
+}
---
+++ src/main/java/com/hi/dhl/utils/TimeUtils.kt
@@ -0,0 +1,10 @@
+package com.hi.dhl.utils
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2022/10/2
+ *     desc  : 
+ * </pre>
+ */class TimeUtils {
+}
