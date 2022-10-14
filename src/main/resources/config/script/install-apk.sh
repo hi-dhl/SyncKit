7070656d52366728b566bc8c5a88023eef7122cf
hi-dhl <hihuilong@gmail.com>
2022-10-14 15:25:31 +0800

    upgrad version to 1.8

--- src/main/resources/config/script/install-apk.sh
+++ src/main/resources/config/script/install-apk.sh
@@ -2,7 +2,8 @@
 launchActivtiy=$1
 find . -name "*.apk" | while read fname; do
   echo $fname
-  adb install -r $fname
+#  -r : 覆盖原来安装的 APK 并保留数据
+  adb install -r -t $fname
   if [ "$launchActivtiy" ]; then
     adb shell am start $launchActivtiy
   fi
