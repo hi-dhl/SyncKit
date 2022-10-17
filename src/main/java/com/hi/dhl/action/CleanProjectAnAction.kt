0af0761aace461cefd13d7c88510960a19bc9b4e
hi-dhl <hihuilong@gmail.com>
2022-10-17 10:55:39 +0800

    修复clean路径找不到的问题

--- src/main/java/com/hi/dhl/action/CleanProjectAnAction.kt
+++ src/main/java/com/hi/dhl/action/CleanProjectAnAction.kt
@@ -2,7 +2,11 @@ package com.hi.dhl.action
 
 import com.hi.dhl.action.base.AbstractExecShellAction
 import com.hi.dhl.common.R
+import com.hi.dhl.console.CommandManager
+import com.hi.dhl.utils.FileUtils
+import com.hi.dhl.utils.LogUtils
 import com.intellij.openapi.project.Project
+import java.io.File
 
 /**
  * <pre>
@@ -14,7 +18,24 @@ import com.intellij.openapi.project.Project
 class CleanProjectAnAction : AbstractExecShellAction(R.String.ui.actionCleanProject) {
 
     override fun afterActionPerformed(project: Project) {
-        execShelLScript(project, R.ShellScript.cleanProject)
+        val src = FileUtils.getShellScriptPath(projectBasePath, R.ShellScript.cleanProject)
+        val dest = FileUtils.getShellScriptDirPath(projectBasePath)
+        if (!FileUtils.isExists(src)) {
+            FileUtils.copyScriptFile(R.ShellScript.cleanProject, File(dest))
+        }
+
+        if (FileUtils.isExists(src)) {
+            execShelLScript(project, R.ShellScript.cleanProject)
+        }else{
+            val extraCommand = "./gradlew --stop; rm -rf ~/.gradle/caches/build-cache-*; ./gradlew clean"
+            val commands = StringBuilder()
+            val projectName = projectBasePath.substring(projectBasePath.lastIndexOf(File.separator) + 1)
+            val remoteProjectPath = remoteMachineInfo.remoteRootDir + File.separator + projectName
+            CommandManager.execRemoteCommand(commands, remoteProjectPath, extraCommand, remoteMachineInfo)
+            val consoleTitle = "${R.String.projectTitle} [ clean ] "
+            execSyncRunnerConsole(project, projectBasePath, commands.toString(), consoleTitle)
+        }
+
     }
 
 }
